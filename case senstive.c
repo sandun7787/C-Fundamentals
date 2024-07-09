8bebdef255291e3bccfa76899821de4bd59a41ef
sandun7787 <sandunwarnasooreya345@gmail.com>
2024-07-09 19:38:03 +0530

    add-new-real-worlds-example

---
+++ case senstive.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+int newMinute=50;
+
+int new=60;
+printf("%d/n",newMinute);
+printf("%d", new);
+return 0;
+}
---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+//student ages
+int studentId=45;
+int studentAges=24;
+float studentFees= 75.45;
+char studentGrade = "B";
+
+//print variabe
+printf("student id is %d/n" , studentId);
+printf("student ages is %d/n", studentAges);
+printf("student fees is %d" , studentFees);
+printf("student grade is %d" , studentGrade);
+return 0;
+
+
+}
