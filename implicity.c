f5c0cca7e6424dc4129b593ed973217a54c35c58
sandun7787 <sandunwarnasooreya345@gmail.com>
2024-07-12 08:45:27 +0530

    add-new-implicity-and-complicity

---
+++ implicity.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+float new=9;
+
+printf("%f", new);
+return 0;
+}
---
+++ typeConversions.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+int x=10;
+int y=20;
+
+int sum =x+y;
+printf("%d",sum);
+return 0;
+}
