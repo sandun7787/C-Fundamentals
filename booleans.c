6ede064885f286438c33151f26858b57d6d30fda
sandun7787 <sandunwarnasooreya345@gmail.com>
2024-07-12 10:28:15 +0530

    add/real/woorlds/projects

---
+++ booleans.c
@@ -0,0 +1,11 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+int main(){
+bool isProgramingFun =true;
+bool isNotprogramingFun =false;
+
+printf("%d\n",isProgramingFun)'
+printf("%d\n",isNotprogramingFunPrograming);
+return 0;
+}
---
+++ realWorld.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+int myAge=34;
+int mySis=56;
+
+printf("%d\n",myAge<mySis);
+return 0;
+}
