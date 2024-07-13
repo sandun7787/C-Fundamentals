5304f54076307cebb3a02195809eb0c96c024617
sandun7787 <sandunwarnasooreya345@gmail.com>
2024-07-13 09:18:11 +0530

    c-fundamentals

---
+++ ifelse.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+int x = 20;
+int y = 30;
+
+if(x<y){
+    printf("x is less than y");
+}
+return 0;
+}
---
+++ include.c
@@ -0,0 +1,10 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+int main(){
+if(20<30){
+printf("20 less than 30");
+
+}
+return 0;
+}
