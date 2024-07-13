dda59ce1ef09824dacdfe90d4c2587acf85fd286
sandun7787 <sandunwarnasooreya345@gmail.com>
2024-07-13 09:59:23 +0530

    realworld

---
+++ Terneryoperator.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+int time =20;
+(time<50)?printf("Good day"): printf("good evening");
+return 0;
+}
---
+++ ifElsereallife.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+int doorCode = 1337;
+
+if (doorCode==1337){
+    printf("Correct code.\nThe door is now open");
+
+}else{
+    printf("Wrong code.\nThe door remains closed");
+
+}
+return 0;
+}
