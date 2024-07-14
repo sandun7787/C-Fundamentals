28879f67ef0ec7437ddc66730a89aac12173e5c0
sandun7787 <sandunwarnasooreya345@gmail.com>
2024-07-14 10:33:39 +0530

    add-new-realworld

---
+++ switchStatments.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+ int main(){
+     int day=4;
+ switch(day){
+     case 1;
+     printf("monday");
+     break;
+     case 2;
+     printf("tuesday");
+     break;
+     case3;
+     printf("wednesday");
+     break;
+     case 4;
+     printf("Friday");
+     break;
+
+ }
+ return 0;
+ }
---
+++ whileloopRealworld.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+int countDown=3;
+while(countDown< 0){
+        printf("%d\n"countDown)
+        countDown---
+
+
+}
+printf("happy new year\n")
+return 0;
+}
